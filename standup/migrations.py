7e605dff08800fec63fd159ad0fb93cd58bc3d05
Rehan Dalal <rehandalal@gmail.com>
2012-08-07 11:23:22 -0400

    Fix paths in migrations.py

--- standup/migrations.py
+++ standup/migrations.py
@@ -2,9 +2,11 @@
 import os
 from migrate.versioning.shell import main
 
-db_url = os.environ.get('DATABASE_URL', 'sqlite:///standup_app.db')
-repo_path = os.path.dirname(os.path.abspath(__file__))
-repository = os.path.join(repo_path, './migrations')
+app_path = os.path.relpath(os.path.dirname(os.path.abspath(__file__)),
+                           os.getcwd())
+sqlite = os.path.join(app_path, 'standup_app.db')
+repository = os.path.join(app_path, 'migrations')
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///%s' % sqlite)
 
 if __name__ == '__main__':
     main(url=db_url, repository=repository)
