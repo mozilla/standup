2d21e679bd40aab62a4efa7d7a5c9fb88e7ce726
Rehan Dalal <rehandalal@gmail.com>
2012-08-07 02:18:19 -0400

    Fix migrations.py to locate repo correctly

--- standup/migrations.py
+++ standup/migrations.py
@@ -3,6 +3,9 @@ import os
 from migrate.versioning.shell import main
 
 db_url = os.environ.get('DATABASE_URL', 'sqlite:///standup_app.db')
+repo_path = os.path.dirname(os.path.abspath(__file__))
+repository = os.path.join(repo_path, './migrations')
 
 if __name__ == '__main__':
-    main(url=db_url, repository='migrations')
+    print repo_path
+    main(url=db_url, repository=repository)
