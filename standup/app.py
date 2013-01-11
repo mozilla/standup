9d4775e87da1a78d3bfa3019eed5dc1e1b857c8d
Nikos Roussos <nikos@roussos.cc>
2013-01-11 23:16:11 +0200

    Add a read-only API

--- standup/app.py
+++ standup/app.py
@@ -305,6 +305,42 @@ def profile():
         statuses=user.recent_statuses())
 
 
+@app.route('/api/v1/status/<limit>/', methods=['GET'])
+def get_statuses(limit):
+    """Get all status updates.
+
+    Returns id, user (the name), project name and timestamp of statuses.
+    The amount of items to return is determined by the limit attribute.
+
+    An example of the JSON:
+
+        {
+            "1": {
+                "user": "r1cky",
+                "content": "working on bug 123456",
+                "project": "sumodev",
+                "timestamp": "2013-01-11T21:13:30.806236"
+            }
+        }
+    """
+
+    statuses = Status.query.filter(Status.reply_to == None). \
+             order_by(db.desc(Status.created)).limit(limit)
+
+    data={}
+    for row in statuses:
+        id = row.id
+        created = row.created.isoformat()
+        try:
+            project_name = row.project.name
+        except:
+            project_name = None
+        data[id] = (dict(author=row.user.name, content=row.content,
+                         timestamp=created, project=project_name))
+
+    return jsonify(data)
+
+
 @app.route('/api/v1/status/', methods=['POST'])
 @api_key_required
 def create_status():
