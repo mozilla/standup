535c1a451c2d7db0409025d052922b5f87652b2c
Rehan Dalal <rehandalal@gmail.com>
2013-02-05 16:31:51 -0500

    Fix day view

--- standup/apps/status/helpers.py
+++ standup/apps/status/helpers.py
@@ -18,14 +18,14 @@ def startdate(request):
     elif dates == 'today':
         return date.today()
     elif isday(day):
-        return day(day)
+        return get_day(day)
     return None
 
 
 def enddate(request):
     day = request.args.get('day')
     if isday(day):
-        return day(day) + timedelta(days=1)
+        return get_day(day) + timedelta(days=1)
     return None
 
 
@@ -33,5 +33,5 @@ def isday(day):
     return day and re.match('^\d{4}-\d{2}-\d{2}$', day)
 
 
-def day(day):
+def get_day(day):
     return datetime.strptime(day, '%Y-%m-%d')
