f61f333b50083170d41bb91f765a3bcd392e50d7
Jannis Leidel <jannis@leidel.info>
2014-04-25 17:33:27 +0200

    Addd @<username> syntax for status formatting.

--- standup/tests/test_filters.py
+++ standup/tests/test_filters.py
@@ -1,44 +1,44 @@
-from flask import Flask
 from nose.tools import eq_
 from standup.filters import format_update, gravatar_url, TAG_TMPL
+from standup.tests import BaseTestCase
 
 
-def test_tags():
-    """Test that format update parses tags correctly"""
-    # Test valid tags.
-    for tag in ('#t', '#tag', '#TAG', '#tag123'):
-        expected = '%s <div class="tags">%s</div>' % (
-            tag, TAG_TMPL.format('', tag[1:].lower(), tag[1:]))
-        eq_(format_update(tag), expected)
+class FilterTestCase(BaseTestCase):
+    def test_tags(self):
+        """Test that format update parses tags correctly"""
+        with self.app.app_context():
+            # Test valid tags.
+            for tag in ('#t', '#tag', '#TAG', '#tag123'):
+                expected = '%s <div class="tags">%s</div>' % (
+                    tag, TAG_TMPL.format('', tag[1:].lower(), tag[1:]))
+                eq_(format_update(tag), expected)
 
-    # Test invalid tags.
-    for tag in ('#1', '#.abc', '#?abc'):
-        eq_(format_update(tag), tag)
+            # Test invalid tags.
+            for tag in ('#1', '#.abc', '#?abc'):
+                eq_(format_update(tag), tag)
 
+    def test_gravatar_url(self):
+        """Test that the gravatar url is generated correctly"""
+        with self.app.test_request_context('/'):
+            self.app.debug = True
+            url = gravatar_url('test@test.com')
+            eq_(url, 'http://www.gravatar.com/avatar/'
+                     'b642b4217b34b1e8d3bd915fc65c4452?d=mm')
 
-def test_gravatar_url():
-    """Test that the gravatar url is generated correctly"""
-    app = Flask(__name__)
+            url = gravatar_url('test@test.com', 200)
+            eq_(url, 'http://www.gravatar.com/avatar/'
+                     'b642b4217b34b1e8d3bd915fc65c4452?s=200&d=mm')
 
-    with app.test_request_context('/'):
-        app.debug = True
-        url = gravatar_url('test@test.com')
-        eq_(url, 'http://www.gravatar.com/avatar/'
-                 'b642b4217b34b1e8d3bd915fc65c4452?d=mm')
+            self.app.debug = False
 
-        url = gravatar_url('test@test.com', 200)
-        eq_(url, 'http://www.gravatar.com/avatar/'
-                 'b642b4217b34b1e8d3bd915fc65c4452?s=200&d=mm')
+            url = gravatar_url('test@test.com')
+            eq_(url, 'http://www.gravatar.com/avatar/'
+                     'b642b4217b34b1e8d3bd915fc65c4452?d=mm')
 
-        app.debug = False
+            self.app.config['SITE_URL'] = 'http://www.site.com'
 
-        url = gravatar_url('test@test.com')
-        eq_(url, 'http://www.gravatar.com/avatar/'
-                 'b642b4217b34b1e8d3bd915fc65c4452?d=mm')
-
-        app.config['SITE_URL'] = 'http://www.site.com'
-
-        url = gravatar_url('test@test.com')
-        eq_(url, 'http://www.gravatar.com/avatar/'
-            'b642b4217b34b1e8d3bd915fc65c4452'
-            '?d=http%3A%2F%2Fwww.site.com%2Fstatic%2Fimg%2Fdefault-avatar.png')
+            url = gravatar_url('test@test.com')
+            eq_(url, 'http://www.gravatar.com/avatar/'
+                'b642b4217b34b1e8d3bd915fc65c4452'
+                '?d=http%3A%2F%2Fwww.site.com%2Fstatic%2Fimg%2F'
+                'default-avatar.png')
