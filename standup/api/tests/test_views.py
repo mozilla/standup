a329b026ba2de97e65cd674eda26a4ed49822af3
Will Kahn-Greene <willkg@mozilla.com>
2016-09-05 08:07:38 -0400

    Implement api-status-delete and api-update-user API endpoints

--- standup/api/tests/test_views.py
+++ standup/api/tests/test_views.py
@@ -5,8 +5,16 @@ from django.utils.encoding import force_bytes
 from django.test import Client, TestCase
 
 from standup.api.tests.factories import SystemTokenFactory
-from standup.status.models import Project, Status
-from standup.status.tests.factories import ProjectFactory, StatusFactory, StandupUserFactory
+from standup.status.models import (
+    Project,
+    Status,
+    StandupUser,
+)
+from standup.status.tests.factories import (
+    ProjectFactory,
+    StatusFactory,
+    StandupUserFactory,
+)
 
 
 def _content_generator():
@@ -45,6 +53,11 @@ class JSONClient(Client):
     def post_json(self, path, payload, secure=False, **extra):
         payload = force_bytes(json.dumps(payload))
         return self.generic('POST', path, payload, content_type='application/json',
+                            secure=secure, **extra)
+
+    def delete_json(self, path, payload, secure=False, **extra):
+        payload = force_bytes(json.dumps(payload))
+        return self.generic('DELETE', path, payload, content_type='application/json',
                             secure=secure, **extra)
 
 
@@ -232,3 +245,124 @@ class TestStatusPost(APITestCase):
         )
         assert resp.status_code == 403
         assert resp.content == b'{"error": "Api key forbidden."}'
+
+
+class TestStatusDelete(APITestCase):
+    def test_delete(self):
+        token = SystemTokenFactory.create()
+        standupuser = StandupUserFactory.create()
+        status = StatusFactory.create(user=standupuser)
+
+        resp = self.client.delete_json(
+            reverse('api-status-delete', kwargs={'pk': str(status.id)}),
+            payload={
+                'api_key': token.token,
+                'user': standupuser.username,
+            }
+        )
+        assert resp.status_code == 200
+        assert resp.content == ('{"id": %s}' % (status.id,)).encode('utf-8')
+
+    def test_invalid_username(self):
+        token = SystemTokenFactory.create()
+        standupuser = StandupUserFactory.create()
+        status = StatusFactory.create(user=standupuser)
+
+        resp = self.client.delete_json(
+            reverse('api-status-delete', kwargs={'pk': str(status.id)}),
+            payload={
+                'api_key': token.token,
+                'user': ''
+            }
+        )
+        assert resp.status_code == 400
+        assert resp.content == b'{"error": "Missing required fields."}'
+
+    def test_status_does_not_exist(self):
+        token = SystemTokenFactory.create()
+        standupuser = StandupUserFactory.create()
+
+        resp = self.client.delete_json(
+            reverse('api-status-delete', kwargs={'pk': str(1000)}),
+            payload={
+                'api_key': token.token,
+                'user': standupuser.username,
+            }
+        )
+        assert resp.status_code == 400
+        assert resp.content == b'{"error": "Status does not exist."}'
+
+    def test_wrong_user(self):
+        token = SystemTokenFactory.create()
+        standupuser = StandupUserFactory.create(user__username='charlie')
+        status = StatusFactory.create(user=standupuser)
+
+        resp = self.client.delete_json(
+            reverse('api-status-delete', kwargs={'pk': str(status.id)}),
+            payload={
+                'api_key': token.token,
+                'user': 'fred',
+            }
+        )
+        assert resp.status_code == 403
+        assert resp.content == b'{"error": "You cannot delete this status."}'
+
+
+class TestUpdateUser(APITestCase):
+    def test_update_name(self):
+        token = SystemTokenFactory.create()
+        standupuser = StandupUserFactory.create(name='Data')
+
+        resp = self.client.post_json(
+            reverse('api-update-user', kwargs={'username': standupuser.username}),
+            payload={
+                'api_key': token.token,
+                'name': 'Lor',
+            }
+        )
+        assert resp.status_code == 200
+        standupuser = StandupUser.objects.get(pk=standupuser.id)
+        assert standupuser.name == 'Lor'
+
+    def test_update_email(self):
+        token = SystemTokenFactory.create()
+        standupuser = StandupUserFactory.create(email='data@example.com')
+
+        resp = self.client.post_json(
+            reverse('api-update-user', kwargs={'username': standupuser.username}),
+            payload={
+                'api_key': token.token,
+                'email': 'lor@example.com',
+            }
+        )
+        assert resp.status_code == 200
+        standupuser = StandupUser.objects.get(pk=standupuser.id)
+        assert standupuser.email == 'lor@example.com'
+
+    def test_update_github_handle(self):
+        token = SystemTokenFactory.create()
+        standupuser = StandupUserFactory.create(github_handle='data')
+
+        resp = self.client.post_json(
+            reverse('api-update-user', kwargs={'username': standupuser.username}),
+            payload={
+                'api_key': token.token,
+                'github_handle': 'lor',
+            }
+        )
+        assert resp.status_code == 200
+        standupuser = StandupUser.objects.get(pk=standupuser.id)
+        assert standupuser.github_handle == 'lor'
+
+    def test_user_does_not_exist(self):
+        token = SystemTokenFactory.create()
+
+        resp = self.client.post_json(
+            reverse('api-update-user', kwargs={'username': 'cmdrdata'}),
+            payload={
+                'api_key': token.token,
+                'name': 'lor',
+            }
+        )
+        assert resp.status_code == 400
+        assert resp.content == b'{"error": "User does not exist."}'
