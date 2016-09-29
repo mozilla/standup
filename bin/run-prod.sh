148c8bea56809d56dcca9f78af5c44e5a676ee7a
Paul McLanahan <pmac@mozilla.com>
2016-09-29 10:03:28 -0400

    Fix #210: Add support for New Relic
    Performance monitoring

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
 ./bin/run-common.sh
-exec gunicorn standup.wsgi:application -b 0.0.0.0:${PORT:-8000} --log-file -
+exec newrelic-admin run-program gunicorn standup.wsgi:application -b 0.0.0.0:${PORT:-8000} --log-file -
