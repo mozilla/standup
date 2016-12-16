1b5d7ac3829d1d3c763692dc556b833809ecdb99
Will Kahn-Greene <willkg@mozilla.com>
2016-12-16 17:04:00 -0500

    Try fixing tests again

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -ex
 
-export DATABASE_URL=sqlite:// SECRET_KEY=itsasekrit
+export DATABASE_URL=sqlite://
+export SECRET_KEY=itsasekrit
+export AUTH0_CLIENT_ID=foo
+export AUTH0_CLIENT_SECRET=foo
+export AUTH0_DOMAIN=foo
+export AUTH0_CALLBACK_URL=foo
 
 flake8
 py.test $@
