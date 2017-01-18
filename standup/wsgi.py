5549af8fd6213fbe849e3d2578290bc8616360ab
Will Kahn-Greene <willkg@mozilla.com>
2017-01-18 15:59:22 -0500

    Add monkeypatch to prevent connection closing for memcachier

--- standup/wsgi.py
+++ standup/wsgi.py
@@ -9,10 +9,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+from django.core.cache.backends.memcached import BaseMemcachedCache
 from django.core.wsgi import get_wsgi_application
 
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
+
+# Fix django closing connection to MemCachier after every request (#11331)
+# per the MemCachier docs.
+BaseMemcachedCache.close = lambda self, **kwargs: None
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "standup.settings")
 
