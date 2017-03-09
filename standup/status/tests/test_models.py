c62ac2887fecfd1fa85d017ab2e92f91bf19dc3d
Will Kahn-Greene <willkg@mozilla.com>
2017-03-08 20:38:50 -0500

    Update to Bleach 2.0
    This updates Standup to use Bleach 2.0 and html5lib 0.999999999 (9 9s). It
    switches to reusing a Cleaner and Linker which is an easy performance gain. It
    also adds the nofollow linkify callback so that all status message links are
    marked as "nofollow".

--- standup/status/tests/test_models.py
+++ standup/status/tests/test_models.py
@@ -21,30 +21,34 @@ def test_tags():
 def test_status_htmlify():
     # plain olde markdown links should work
     original = '[The Dude](https://example.com/dude)'
-    expected = '<p><a href="https://example.com/dude">The Dude</a></p>'
+    expected = '<p><a href="https://example.com/dude" rel="nofollow">The Dude</a></p>'
     assert StatusFactory(content=original).htmlify() == expected
 
     # bare urls should work as well, and be truncated
     original = 'My site is https://example.com/the-dude-abides-man'
-    expected = '<p>My site is <a href="https://example.com/the-dude-abides-man">' \
+    expected = '<p>My site is <a href="https://example.com/the-dude-abides-man" rel="nofollow">' \
                'https://example.com/the-dude-a...</a></p>'
     assert StatusFactory(content=original).htmlify() == expected
 
     # https://github.com/mozilla/standup/issues/321
     original = '[#1234](https://example.com/dude)'
-    expected = '<p><a href="https://example.com/dude">#1234</a></p>'
+    expected = '<p><a href="https://example.com/dude" rel="nofollow">#1234</a></p>'
     assert StatusFactory(content=original).htmlify() == expected
 
     # bugzilla linking
     original = 'Bug 1234, bug #5678'
-    expected = '<p><a href="http://bugzilla.mozilla.org/show_bug.cgi?id=1234">Bug 1234</a>, ' \
-               '<a href="http://bugzilla.mozilla.org/show_bug.cgi?id=5678">bug #5678</a></p>'
+    expected = (
+        '<p>'
+        '<a href="http://bugzilla.mozilla.org/show_bug.cgi?id=1234" rel="nofollow">Bug 1234</a>, '
+        '<a href="http://bugzilla.mozilla.org/show_bug.cgi?id=5678" rel="nofollow">bug #5678</a>'
+        '</p>'
+    )
     assert StatusFactory(content=original).htmlify() == expected
 
     # github linking
     original = 'pr 1234, issue #5678'
-    expected = '<p><a href="{0}pull/1234">pr 1234</a>, ' \
-               '<a href="{0}issues/5678">issue #5678</a></p>'
+    expected = '<p><a href="{0}pull/1234" rel="nofollow">pr 1234</a>, ' \
+               '<a href="{0}issues/5678" rel="nofollow">issue #5678</a></p>'
     status = StatusFactory(content=original)
     expected = expected.format(status.project.repo_url)
     assert status.htmlify() == expected
@@ -52,7 +56,7 @@ def test_status_htmlify():
     # user linking
     StandupUserFactory(user__username='dude')
     original = 'phone\'s ringing @dude'
-    expected = '<p>phone\'s ringing <a href="/user/dude/">@dude</a></p>'
+    expected = '<p>phone\'s ringing <a href="/user/dude/" rel="nofollow">@dude</a></p>'
     assert StatusFactory(content=original).htmlify() == expected
     # not a user
     original = 'phone\'s ringing @bunny'
